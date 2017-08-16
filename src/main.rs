1bb7b28a878f7a54ddcb38bebc9f19f5c446eb3c
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-08-16 16:57:54 -0400

    Add methods for writing text

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,10 @@ use canvas::*;
 use utils::*;
 use ColorCode::*;
 
-use std::thread;
-use std::time::Duration;
-
 fn main() {
     let mut can = Canvas::new(15, 15, pix('~', Red, Green));
     
-    let mut x = 2; let mut y = 0;
+    can.text("foojfhddddhjfdhjfdhjfhjfdhjdfhjfdhjdf\nbar\nxyzzy\rbaz", 1, 2, Color::new(Cyan, Black));
     
-    let width = 3; let height = 2;
-    
-    loop {
-        can.rect(x, y, width, height, pix('#', Cyan, Black));
-        
-        println!("{}", can);
-
-        x += 1; y += 1;
-
-        thread::sleep(Duration::new(1, 0));
-    }
+    println!("{}", can);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -22,10 +22,68 @@ impl CanvasUtils for Canvas {
             }
         }
     }
+    
+    unsafe fn text_unchecked(&mut self, text: &str, x: usize, y: usize, color: Color) {
+        let mut current_x = x; let mut current_y = y;
+        
+        for letter in text.chars() {
+            match letter {
+                '\n' => {
+                    current_x = x;
+                    current_y += 1;
+                },
+                '\r' => {
+                    current_x = x;
+                },
+                letter => {
+                    *self.get_unchecked_mut(current_x, current_y) = Pixel::new(letter, color);
+                    
+                    current_x += 1;
+                }
+            }
+        }
+    }
+
+    fn text(&mut self, text: &str, x: usize, y: usize, color: Color) {
+        let mut current_x = x; let mut current_y = y;
+        
+        let mut in_bounds = x < self.width && y < self.height;
+
+        for letter in text.chars() {
+            match letter {
+                '\n' => {
+                    current_x = x;
+                    current_y += 1;
+                    in_bounds = current_x < self.width && current_y < self.height;
+                },
+                '\r' => {
+                    current_x = x;
+                    in_bounds = current_x < self.width && current_y < self.height;
+                },
+                letter => {
+                    if in_bounds {
+                        unsafe {
+                            *self.get_unchecked_mut(current_x, current_y) = Pixel::new(letter, color);
+                        }
+
+                        current_x += 1;
+                        
+                        if self.width <= current_x {
+                            in_bounds = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 pub trait CanvasUtils {
     unsafe fn rect_unchecked(&mut self, x: usize, y: usize, width: usize, height: usize, filler: Pixel);
     
     fn rect(&mut self, x: usize, y: usize, width: usize, height: usize, filler: Pixel);
+
+    unsafe fn text_unchecked(&mut self, text: &str, x: usize, y: usize, color: Color);
+    
+    fn text(&mut self, text: &str, x: usize, y: usize, color: Color);
 }
